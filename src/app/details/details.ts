3fbe7659df8486420593450d91ec87a42a35f828
Marek Lewandowski <lewymarek1221@gmail.com>
2025-12-04 14:55:30 +0100

    add routing and details component functionality

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
-export class Details {
+export class Details implements OnInit {
+  personId?: string;
+  private route = inject(ActivatedRoute);
 
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.personId = params['id'];
+    });
+  }
 }
