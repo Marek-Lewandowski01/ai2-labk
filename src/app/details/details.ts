2c11833e7e62f4d65e3d6e9dc5382fd2778c6fe3
Marek Lewandowski <lewymarek1221@gmail.com>
2025-12-04 15:29:00 +0100

    add details component with person information display and error handling

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,19 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Person } from '../person';
+import { PersonLs } from '../person-ls';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
 export class Details implements OnInit {
-  personId?: string;
+  personId?: number;
+  person?: Person;
   private route = inject(ActivatedRoute);
+  private personLsService = inject(PersonLs);
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.personId = params['id'];
+      this.person = this.personId ? this.personLsService.getPerson(this.personId) : undefined;
     });
   }
 }
