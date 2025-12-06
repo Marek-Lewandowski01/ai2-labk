473640be8c0829254670fbcb2ac2aefbf2bd8bd5
MarekLewy12 <lewarmarko@gmail.com>
2025-12-06 13:45:55 +0100

    add add-person component for person entry with form validation

--- src/app/add-person/add-person.ts
+++ src/app/add-person/add-person.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Person } from '../person';
+import { PersonLs } from '../person-ls';
 
 @Component({
   selector: 'app-add-person',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-person.html',
   styleUrl: './add-person.css',
 })
 export class AddPerson {
+  person: Person = {
+    address: {}
+  };
+  private personLsService = inject(PersonLs);
+  private router = inject(Router);
 
+  save() {
+    this.personLsService.addPerson(this.person);
+    this.router.navigateByUrl('/list');
+  }
 }
